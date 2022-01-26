eee9b083daa73d694cc3c51073cf4694e2c2b978
Christian Kassahun <christes7@gmail.com>
2022-01-27 01:29:40 +0300

    Added signup route and logic

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
+var authRouter = require('./routes/auth')
 
 var app = express();
 
@@ -16,7 +17,7 @@ app.use(cookieParser());
 
 
 app.use('/ping', indexRouter);
-
+app.use('/auth', authRouter)
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
